6c926920fb3b342d205f1e15f73216074c44c698
Gunbard <gunbard@gmail.com>
2015-05-23 20:12:46 -0500

    Resize game when window resizes

--- chengine/engine.js
+++ chengine/engine.js
@@ -25,6 +25,52 @@ DIRECTION_WEST = 90;
 // Mouse coordinates adjusted for canvas position in the page
 mouseX = 0;
 mouseY = 0;
+
+/**
+ Resizes the game to fit the window while keeping the correct aspect ratio
+ */
+function resizeGame() 
+{
+    var gameArea = document.getElementById('enchant-stage');
+    var widthToHeight = 16 / 9;
+    var newWidth = window.innerWidth;
+    var newHeight = window.innerHeight;
+    var newWidthToHeight = newWidth / newHeight;
+    
+    if (newWidthToHeight > widthToHeight) {
+        newWidth = newHeight * widthToHeight;
+        gameArea.style.height = newHeight + 'px';
+        gameArea.style.width = newWidth + 'px';
+    } else {
+        newHeight = newWidth / widthToHeight;
+        gameArea.style.width = newWidth + 'px';
+        gameArea.style.height = newHeight + 'px';
+    }
+    
+    var gameCanvasContainer = document.getElementById('3d-stage');
+    gameCanvasContainer.style.width = gameArea.style.width;
+    gameCanvasContainer.style.height = gameArea.style.height;
+    
+    var gameCanvas = gameCanvasContainer.getElementsByTagName('canvas')[0];
+    var style = gameCanvas.getAttribute('style') || '';
+
+    var scale = {x: 1, y: 1};
+    scale.x = newWidth / gameCanvas.width;
+    scale.y = newHeight / gameCanvas.height;
+    
+    if (scale.x < scale.y) {
+        scaleString = scale.x + ', ' + scale.x;
+    } else {
+        scaleString = scale.y + ', ' + scale.y;
+    }
+    
+    gameCanvas.setAttribute('style', style + ' ' + '-ms-transform-origin: left top; -webkit-transform-origin: left top; -moz-transform-origin: left top; -o-transform-origin: left top; transform-origin: left top; -ms-transform: scale(' + scaleString + '); -webkit-transform: scale3d(' + scaleString + ', 1); -moz-transform: scale(' + scaleString + '); -o-transform: scale(' + scaleString + '); transform: scale(' + scaleString + ');');
+    
+    enchant.Core.instance.scale = scale.x;
+}
+
+window.addEventListener('resize', resizeGame, false);
+window.addEventListener('orientationchange', resizeGame, false);
   
 /**
  Initializes the game. Immediately called once enchant is finished initializing.
